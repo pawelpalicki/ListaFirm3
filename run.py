0c7cc2f7ddaa2914b71b3d6054966cfbde74e6e6
pawelpalicki <pawelpalicki@wp.pl>
2025-04-17 07:39:08 +0000

    add requirements.txt

--- run.py
+++ run.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
     with flask_app.app_context():
         # Registers all models with SQLAlchemy
         import app.models
-    flask_app.run(debug=True)
+    flask_app.run(host='0.0.0.0',debug=True)
